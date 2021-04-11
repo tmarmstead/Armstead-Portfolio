a1865220cc96077c76d7304d5832751f1ff6dda3
Tina Armstead <tina.armstead@gmail.com>
2021-04-11 12:42:00 -0400

    Updated package.json for deployment

--- armstead-portfolio/src/App.js
+++ armstead-portfolio/src/App.js
@@ -29,7 +29,7 @@ READY? GET SET? UNWIND!" img={entertainment} />
       <Project title="Employee Directory" text="This app allows users to view and entire employee directory for quick access to employee information." img={employeeDirectory} />
       <Project title="Express Handlebars" text="This app uses express handlebars to manipulate data from the browser." img={burger} />
       <Project title="Grocery List Helper" text="The Grocery list application will help any user upon all their grocery excursions. The user can simply log what they need to buy onto their list and once it's purchased, they will be able to move it to their fridge list. The user has the ability to move it back and forth for future grocery trips, and may also delete the item if it's no longer needed. On top of the grocery list, the user will be able to search up recipe ideas based on the ingredients they have/need." img={grocery} />
-      <Project title="Grocery List Helper" text="" img={budgetTracker} />
+      <Project title="Budget Tracker" text="This app is designed to allow for offline access and functionality. It lets a user add or subtract and keep track of their transactions both on and offline." img={budgetTracker} />
       <Footer text="In programming, the hard part isn't solving problems, but deciding what problems to solve." person="-Paul GrahaThis app is designed to allow for offline access and functionality. It lets a user add or subtract and keep track of their transactions both on and offline.m" />
     </div>
   );
