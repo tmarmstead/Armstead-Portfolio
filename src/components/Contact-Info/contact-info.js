65e1ea73b46b272fa4bbc14db0bff3f4b012a6b7
Titina Armstead <71151032+tmarmstead@users.noreply.github.com>
2021-05-11 19:11:12 -0400

    Added Icons and links to contact card in Carousel

--- src/components/Contact-Info/contact-info.js
+++ src/components/Contact-Info/contact-info.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./contact-info.css";
-import { Carousel, Card, Button, Link } from "react-bootstrap";
-import readme from "../project-images/readme.png";
+import { Carousel, Card, Button, Col, Image } from "react-bootstrap";
+// import readme from "../project-images/readme.png";
 import Profile from "../project-images/profile_photo.png";
 import letsConnect from "../project-images/connect.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 
 function ContactCarousel() {
     return (
@@ -21,17 +25,22 @@ function ContactCarousel() {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <Card style={{ width: '18rem' }} className="profile-pic">
+                    <Card className="contact-info">
                         <Card.Body>
-                            <Card.Title>Find me on LinkedIn</Card.Title>
-
-                            <Button variant="primary" onClick="www.google.com">Go somewhere</Button>
-
-                            <Card.Title>Find me on GitHub</Card.Title>
-
-                            <Button variant="primary" onClick="www.google.com">Go somewhere</Button>
-
-
+                            {/* <Card.Title className="card-title">Find me on LinkedIn</Card.Title> */}
+                            <div className="text-center">
+                                <FontAwesomeIcon icon={faLinkedin} className="fa-5x" />
+                            </div>
+                            <div className="text-center">
+                                <Button variant="primary" onClick="www.google.com" className="card-button" href="https://www.linkedin.com/in/tina-armstead-87854498/" target="_blank">View My LinkedIn Profile</Button>
+                            </div>
+                            {/* <Card.Title className="card-title">Find me on GitHub</Card.Title> */}
+                            <div className="text-center">
+                                <FontAwesomeIcon icon={faGithub} className="fa-5x" />
+                            </div>
+                            <div className="text-center">
+                                <Button variant="primary" onClick="www.google.com" className="card-button" href="https://github.com/tmarmstead" target="_blank">View My Github Profile</Button>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Carousel.Item>
